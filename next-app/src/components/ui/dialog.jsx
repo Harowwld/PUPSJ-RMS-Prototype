fe5a885cf3b00c18ac97e60744e6c4643d047509
Paulo Masculino <paulomasculino1022@gmail.com>
2026-06-14 22:34:24 +0800

    Refactor: Remove backdrop blur from DialogOverlay component

--- next-app/src/components/ui/dialog.jsx
+++ next-app/src/components/ui/dialog.jsx
@@ -49,7 +49,7 @@ function DialogOverlay({
     <DialogPrimitive.Backdrop
       data-slot="dialog-overlay"
       className={cn(
-        "fixed inset-0 isolate z-50 bg-black/10 supports-backdrop-filter:backdrop-blur-xs",
+        "fixed inset-0 isolate z-50 bg-black/10",
         className
       )}
       {...props} />
