e325ba86e7c0817760ce6b58473f788939c27b98
Cedrick Joseph Mariano <166236276+icodecedd@users.noreply.github.com>
2026-09-12 09:19:33 +0800

    Revert "perf(webpack): ignore .local, .git, and test directories to prevent unnecessary recompiles"
    This reverts commit 8b6803ec7c0f4b111f9ba050a854e9165cc86998.

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -22,22 +22,6 @@ const nextConfig = {
       "framer-motion",
     ],
   },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        ...config.watchOptions,
-        ignored: [
-          "**/.git/**",
-          "**/node_modules/**",
-          "**/.local/**",
-          "**/.next/**",
-          "**/scripts/**",
-          "**/test/**",
-        ],
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
